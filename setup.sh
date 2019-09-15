335e7432179eac3b41ddfbda18a6f8160fa315fa
jcook3701 <jcook3701@gmail.com>
2019-09-14 19:11:01 -0700

    update

--- setup.sh
+++ setup.sh
@@ -164,8 +164,8 @@ ENTITY=DBG
 # Compiler and library settings:
 # Selects compiler version to use
 PATH=\$PATH:\$STM32BASE/tools/gcc-arm-none-eabi-8-2019-q3-update/bin
-STM32_H7XX_LIBDIR="\$STM32BASE/firmware/stm32H743/lib"
-STM32_L4XX_LIBDIR="\$STM32BASE/firmware/stm32L432/lib"
+STM32_H7XX_LIBDIR="\$STM32BASE/firmware/stm32h743/lib"
+STM32_L4XX_LIBDIR="\$STM32BASE/firmware/stm32l432/lib"
 # Export everything to the environment
 export STM32BASE
 export STM32DEV
